9cc82213437ec1fedabcb6ce5d5af6187c986941
andreagorsche <andrea.gorsche@gmail.com>
2023-05-09 17:34:24 +0000

    Add favicon, add fontawesome icons for menu, swap code of App.js and NavMenu.js, put it the wrong way at first

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
-import React from "react";
-import {Navbar, Container, Nav} from "react-bootstrap";
-import cookbook from "./assets/cookbook.png";
+import "./App.css";
+import NavMenu from "./components/NavMenu";
 
-const NavMenu = () => {
-  return <Navbar collapseOnSelect expand="md" fixed="top" bg="dark" variant="dark"> <Container>
-  <Navbar.Brand href="#home"><img src={cookbook} alt="logo" height="50"/></Navbar.Brand>
-  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-  <Navbar.Collapse id="responsive-navbar-nav">
-    <Nav className="mr-auto">
-      <Nav.Link href="#Recipes">Recipes</Nav.Link>
-      <Nav.Link href="#Login">Login</Nav.Link>
-      <Nav.Link href="#Register">Register</Nav.Link>
-    </Nav>
-  </Navbar.Collapse></Container>
-</Navbar>
-};
-export default NavMenu;
+function App() {
+  return (
+    <div className="App">
+      <NavMenu/>
+      </div>
+  );
+}
+
+export default App;
--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import {Navbar, Container, Nav} from "react-bootstrap";
+import cookbook from "../assets/cookbook.png";
 
-function NavMenu() {
-  return (
-    <div>NavMenu</div>
-  )
-}
-
-export default NavMenu
+const NavMenu = () => {
+  return <Navbar collapseOnSelect expand="md" fixed="top" bg="dark" variant="dark"> <Container>
+  <Navbar.Brand href="#home"><img src={cookbook} alt="logo" height="50"/></Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="ml-auto text-right">
+      <Nav.Link>
+      <i className="fa-solid fa-utensils"></i>
+      Recipes</Nav.Link>
+      <Nav.Link>
+        <i className="fa-regular fa-user"></i>Login
+      </Nav.Link>
+      <Nav.Link >
+        <i className="fa-solid fa-user-plus"></i>Register
+      </Nav.Link>
+    </Nav>
+  </Navbar.Collapse></Container>
+</Navbar>
+};
+export default NavMenu;
