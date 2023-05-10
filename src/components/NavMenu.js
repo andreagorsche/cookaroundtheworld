3ad3eed79229bf61b3212c3195c0688fd04b5083
andreagorsche <andrea.gorsche@gmail.com>
2023-05-10 13:14:03 +0000

    Add Routing to the functionality and debug App.css and NavMenu.module.css

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -2,22 +2,36 @@ import React from "react";
 import {Navbar, Container, Nav} from "react-bootstrap";
 import cookbook from "../assets/cookbook.png";
 import styles from "../styles/NavMenu.module.css"
+import {NavLink} from "react-router-dom";
 
 const NavMenu = () => {
   return <Navbar className={styles.NavMenu} collapseOnSelect expand="md" fixed="top"> <Container>
-  <Navbar.Brand href="#home"><img src={cookbook} alt="logo" height="50"/></Navbar.Brand>
+  <NavLink to = "/">
+    <Navbar.Brand href="#home">
+      <img src={cookbook} alt="logo" height="50"/>
+      </Navbar.Brand>
+    </NavLink>
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="ml-auto text-right">
-      <Nav.Link>
-      <i className="fa-solid fa-utensils"></i>
-      Recipes</Nav.Link>
-      <Nav.Link>
+    <NavLink exact
+    className = {styles.NavLink} 
+    activeClassName = {styles.Active} 
+    to = "/">
+      <i className="fa-solid fa-utensils"></i>Recipes
+      </NavLink>
+      <NavLink 
+      className = {styles.NavLink} 
+      activeClassName = {styles.Active} 
+      to = "/login">
         <i className="fa-regular fa-user"></i>Login
-      </Nav.Link>
-      <Nav.Link >
+      </NavLink>
+      <NavLink 
+      className = {styles.NavLink} 
+      activeClassName = {styles.Active} 
+      to = "/register">
         <i className="fa-solid fa-user-plus"></i>Register
-      </Nav.Link>
+      </NavLink>
     </Nav>
   </Navbar.Collapse></Container>
 </Navbar>
