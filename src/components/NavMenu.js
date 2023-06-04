f2d1566232beaed48a8fda5d1702a3a6e7f52613
andreagorsche <andrea.gorsche@gmail.com>
2023-06-04 21:12:09 +0000

    Debug nested anchor tag bug in index.html

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -5,35 +5,42 @@ import styles from "../styles/NavMenu.module.css"
 import {NavLink} from "react-router-dom";
 
 const NavMenu = () => {
-  return <Navbar className={styles.NavMenu} collapseOnSelect expand="md" fixed="top"> <Container>
-  <NavLink to = "/">
-    <Navbar.Brand href="#home">
-      <img src={cookbook} alt="logo" height="50"/>
-      </Navbar.Brand>
-    </NavLink>
-  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-  <Navbar.Collapse id="responsive-navbar-nav">
-    <Nav className="ml-auto text-right">
-    <NavLink exact
-    className = {styles.NavLink} 
-    activeClassName = {styles.Active} 
-    to = "/">
-      <i className="fa-solid fa-utensils"></i>Recipes
-      </NavLink>
-      <NavLink 
-      className = {styles.NavLink} 
-      activeClassName = {styles.Active} 
-      to = "/login">
-        <i className="fa-regular fa-user"></i>Login
-      </NavLink>
-      <NavLink 
-      className = {styles.NavLink} 
-      activeClassName = {styles.Active} 
-      to = "/register">
-        <i className="fa-solid fa-user-plus"></i>Register
-      </NavLink>
-    </Nav>
-  </Navbar.Collapse></Container>
-</Navbar>
+  return (
+    <Navbar className={styles.NavMenu} collapseOnSelect expand="md" fixed="top"> 
+      <Container>
+        <NavLink to = "/">
+          <Navbar.Brand>
+            <img src={cookbook} alt="logo" height="50"/>
+          </Navbar.Brand>
+        </NavLink>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ml-auto text-right">
+            <NavLink 
+              exact
+              className = {styles.NavLink} 
+              activeClassName = {styles.Active} 
+              to = "/"
+            >
+              <i className="fa-solid fa-utensils"></i>Recipes
+          </NavLink>
+          <NavLink 
+            className = {styles.NavLink} 
+            activeClassName = {styles.Active} 
+            to = "/login">
+            <i className="fa-regular fa-user"></i>Login
+          </NavLink>
+          <NavLink 
+            className = {styles.NavLink} 
+            activeClassName = {styles.Active} 
+            to = "/register">
+            <i className="fa-solid fa-user-plus"></i>Register
+          </NavLink>
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
+  );
 };
+
 export default NavMenu;
