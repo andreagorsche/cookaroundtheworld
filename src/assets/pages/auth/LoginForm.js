f7072bf83faff169f9a949f19f0e8a90a1995c1b
andreagorsche <andrea.gorsche@gmail.com>
2023-06-06 20:35:21 +0000

    create handle submit functionality

--- src/assets/pages/auth/LoginForm.js
+++ src/assets/pages/auth/LoginForm.js
@@ -1,19 +1,14 @@
 import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import { Form, Button, Col, Row, Container, Alert } from "react-bootstrap";
 
-import Form from "react-bootstrap/Form";
-import Alert from "react-bootstrap/Alert";
-import Button from "react-bootstrap/Button";
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
-import Container from "react-bootstrap/Container";
 import chef_group from "../../../assets/chef_group.png"; 
-
-
-import { Link } from "react-router-dom";
 
 import styles from "../../../styles/LoginRegister.module.css";
 import btnStyles from "../../../styles/button.module.css";
 import appStyles from "../../../App.module.css";
+
+import axios from "axios";
 
 function LoginForm() {
     const [loginData, setLoginData] = useState({
@@ -22,6 +17,8 @@ function LoginForm() {
     });
 
     const {username, password} = loginData;
+
+    const history = useHistory();
     
     const handleChange = (event) => {
         setLoginData({
@@ -30,12 +27,21 @@ function LoginForm() {
         });
     };
 
+    const handleSubmit = async (event) => {
+		event.preventDefault();
+		try {
+		  await axios.post("/dj-rest-auth/login/", loginData);
+		  history.push("/");
+		} catch (err) {
+		}
+	  };
+
   return (
     <Row className="justify-content-md-center align-items-center">
       <Col className="my-auto p-0 p-md-2" md={6}>
         <Container className={`${appStyles.Content} p-4 `}>
           <h1 className={styles.Header}>Welcome back!</h1>
-          <Form>
+          <Form onSubmit={handleSubmit}>
             <img src={chef_group} className={`${appStyles.CommunityImage}`} alt="community" height="100"/>
             <h1>Login</h1>
             <Form.Group controlId="Username">
