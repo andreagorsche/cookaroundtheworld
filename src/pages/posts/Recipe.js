d51605dcdec8e1ecc2ce9936b4b90ce36883f5e6
andreagorsche <andrea.gorsche@gmail.com>
2023-06-23 13:05:18 +0000

    Delete recipe.js file due to debugging

--- src/pages/posts/Recipe.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import styles from "../../styles/Recipe.module.css"
-
-export const Recipe = (props) => {
-  return (
-    <div>Recipe Placeholder Text</div>
-  )
-}
-export default Recipe;
