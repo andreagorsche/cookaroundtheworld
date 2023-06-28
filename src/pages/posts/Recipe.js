54ee36de7564dc29f3456d895b26be9ccc57cce4
andreagorsche <andrea.gorsche@gmail.com>
2023-06-28 19:45:32 +0000

    Wire up the basic systematic of the Recipe.js file and RecipePage.js

---
+++ src/pages/posts/Recipe.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from "../../styles/Recipe.module.css";
+
+const Recipe = (props) => {
+  return (
+    <div>Recipe</div>
+  )
+}
+
+export default Recipe   
--- src/pages/posts/RecipePage.js
+++ src/pages/posts/RecipePage.js
@@ -5,6 +5,7 @@ import Container from "react-bootstrap/Container";
 import appStyles from "../../App.module.css";
 import { useParams } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
+import Recipe from "./Recipe";
 
 function RecipePage() {
   const { id } = useParams();
@@ -32,7 +33,7 @@ function RecipePage() {
   return (
     <Row className="justify-content-center" >
       <Col className="py-2 p-0 p-lg-2" lg={8}>
-        <p>Post component</p>
+        <Recipe {...recipe.results[0]} setRecipes={setRecipe} recipePage />
         <Container className={appStyles.Content}>
           Comments
         </Container>
