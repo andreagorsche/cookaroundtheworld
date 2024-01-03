d7c84f793796abfbe80677d2dda3c5688784f317
andreagorsche <andrea.gorsche@gmail.com>
2024-01-03 17:03:53 +0000

    Insert Header and Intro component for recipe page - add the list prop to the intro component

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -4,6 +4,7 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import ImageBlock from './ImageBlock';
 import '../styles/components/Intro.module.css';
+import { withRouter } from 'react-router-dom/cjs/react-router-dom.min';
 
 function Intro({
   firstWord,
@@ -11,42 +12,44 @@ function Intro({
   secondPhrase,
   firstParagraph,
   secondParagraph,
-  listStyle,
-  image1,
-  image2,
+  imageBlock,
+  listItems,
+  heading,
+  timeEffort,
 }) {
   const paragraphStyle = {
     fontFamily: 'AnHanken Grotesk',
-    fontSize: '1.2rem', // Adjust the font size as needed
+    fontSize: '1.2rem',
   };
 
-  const listStyleConstant = {
-    listStyleType: 'disc', // You can customize this as needed
-    marginLeft: '12rem', // Adjust the left margin as needed
+  const rightColumnStyle = {
+    backgroundColor: 'black',
+    padding: '20px', 
+    color: 'white',
   };
 
   return (
     <Container fluid id="Intro2">
       <Row style={{ display: 'flex', alignItems: 'stretch' }}>
-        <Col className='intro-text' md={6} >
+        <Col className='intro-text' md={6}>
           <h1>
             {firstWord} <span style={{ color: '#F6BE00' }}>{secondWord}</span>{' '}
             {secondPhrase}
           </h1>
           <p style={paragraphStyle}>{firstParagraph}</p>
-                    {Array.isArray(listStyle) ? (
-            <ul style={{ ...paragraphStyle, ...listStyleConstant }}>
-              {listStyle.map((item, index) => (
+          <p style={paragraphStyle}>{secondParagraph}</p>
+        </Col>
+        <Col style={rightColumnStyle} md={6}>
+          {imageBlock && <ImageBlock {...imageBlock} />}
+          {timeEffort && <p style={{ ...paragraphStyle, color: 'white' }}>Time Effort: {timeEffort}</p>}
+          <h3>{heading}</h3>
+          {Array.isArray(listItems) && listItems.length > 0 && (
+            <ul style={{ ...paragraphStyle, listStyleType: 'disc' }}>
+              {listItems.map((item, index) => (
                 <li key={index}>{item}</li>
               ))}
             </ul>
-          ) : (
-            <p style={paragraphStyle}>{listStyle}</p>
           )}
-          <p style={paragraphStyle}>{secondParagraph}</p>
-        </Col>
-        <Col className="image-block-design" style={{ padding: 0 }} md={6}>
-          <ImageBlock image1={image1} image2={image2} />
         </Col>
       </Row>
     </Container>
--- src/pages/posts/RecipePage.js
+++ src/pages/posts/RecipePage.js
@@ -1,14 +1,13 @@
 import React, { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { axiosReq } from "../../api/axiosDefaults";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
-import appStyles from "../../App.module.css";
-import { useParams } from "react-router";
-import { axiosReq } from "../../api/axiosDefaults";
-import RecipeCard from "../../components/RecipeCard";
 import Header from "../../components/Header";
-import JoinHeader from "../../assets/images/JoinHeader.jpg"
-
+import Intro from "../../components/Intro";
+import RecipeCard from "../../components/RecipeCard";
+import HeaderImageCircle from "../../components/HeaderImageCircle";
 
 function RecipePage() {
   const { id } = useParams();
@@ -17,31 +16,45 @@ function RecipePage() {
   useEffect(() => {
     const handleMount = async () => {
       try {
-        const [{ data: recipe }] = await Promise.all([
-          axiosReq.get(`/recipes/${id}`),
-        ]);
-        setRecipe({ results: [recipe] });
-        console.log(recipe);
+        const { data } = await axiosReq.get(`/recipes/${id}`);
+        setRecipe({ results: [data] });
+        console.log(data);
       } catch (err) {
         console.log(err);
-            }
+      }
     };
 
     handleMount();
   }, [id]);
 
+  const { title, image, cuisine, ingredients, time_effort, description, updated_at, is_owner, recipePage, comments_count, likes_count, like_id } = recipe.results[0] || {};
+  const ingredientsArray = ingredients ? ingredients.split(',').map(item => item.trim()) : [];
+  const headerImageUrl = recipe.results[0]?.image;
+
+
   return (
     <>
-    <Header imageUrl={JoinHeader} />
-    <Row className="justify-content-center" >
-      <Col className="py-2 p-0 p-lg-2" lg={8}>
-      <RecipeCard {...recipe.results[0]} setRecipes={setRecipe} recipePage />
-        <Container className={appStyles.Content}>
-          Comments
-        </Container>
-      </Col>
-    </Row>
-
+      <Header imageUrl={headerImageUrl} />
+      <Intro
+        firstWord="Cooking"
+        secondWord={title}
+        secondPhrase="with us"
+        firstParagraph={cuisine}
+        secondParagraph={description}
+        heading="Ingredients"
+        timeEffort={time_effort}
+        listItems={ingredientsArray}
+      />
+      <Row className="justify-content-center">
+        <Col className="py-2 p-0 p-lg-2" lg={8}>
+           <Container className="text-center">
+            Rating
+          </Container>
+          <Container className="text-center">
+            Comments
+          </Container>
+        </Col>
+      </Row>
     </>
   );
 }
