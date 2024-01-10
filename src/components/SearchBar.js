5ca393977c6672e76395f1e45847459104e6e3ac
andreagorsche <andrea.gorsche@gmail.com>
2024-01-10 15:38:56 +0000

    Add react select, add array of ingredients with label and value to the search bar component, add handle ingredient change logic to the searchbar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,12 +3,30 @@ import React, { useState, useEffect } from 'react';
 import Dropdown from 'react-bootstrap/Dropdown';
 import { useSetRecipeData } from '../contexts/RecipeDataContext';
 import { axiosReq } from '../api/axiosDefaults';
+import Select from 'react-select';
 
 const SearchBar = () => {
   const setRecipeData = useSetRecipeData();
   const cuisineChoices = ['american', 'austrian', 'caribbean', 'chinese', 'french', 'german', 'greek', 'indian', 'italian', 'mediterranean', 'mexican', 'slovak', 'spanish']
+  const popularIngredients = [
+    { value: 'pasta', label: 'Pasta' },
+    { value: 'potatoes', label: 'Potatoes' },
+    { value: 'rice', label: 'Rice' },
+    { value: 'chicken', label: 'chicken' },
+    { value: 'beef', label: 'Beef' },
+    { value: 'pork', label: 'Pork' },
+    { value: 'lamb', label: 'Lamb' },
+    { value: 'duck', label: 'Duck' },
+    { value: 'tomatoes', label: 'Tomatoes' },
+    { value: 'zucchini', label: 'Zucchini' },
+    { value: 'aubergine', label: 'Aubergine' },
+    { value: 'mushrooms', label: 'Mushrooms' },
+    { value: 'salad', label: 'Salad' },
+    { value: 'brokkoli', label: 'Brokkoli' },
+    { value: 'cheese', label: 'Cheese' },
+  ]
 
-  const handleCuisineChange = async (cuisine) => {
+    const handleCuisineChange = async (cuisine) => {
     try {
       // Fetch recipes with the selected cuisine filter
       const { data } = await axiosReq.get(`/recipes/?cuisine=${cuisine}`);
@@ -22,7 +40,23 @@ const SearchBar = () => {
     }
   };
 
+  const handleIngredientChange = async (ingredients) => {
+   
+    try {
+      
+      // Fetch recipes with the selected ingredient filter
+      const { data } = await axiosReq.get(`/recipes/?ingredients=${ingredients}`);
+      
+      setRecipeData((prevData) => ({
+        ...prevData,
+        results: data.results,
+      }));
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
+  
   return (
     <>
       <Dropdown>
@@ -37,6 +71,12 @@ const SearchBar = () => {
           ))}
         </Dropdown.Menu>
       </Dropdown>
+      <Select
+        isMulti
+        options={popularIngredients}
+        onChange={handleIngredientChange}
+        placeholder="Select Ingredients"
+      />
     </>
   );
 };
