2d09c0807b93cfe286fd1a4b98d53cc1ee6fbbaa
andreagorsche <andrea.gorsche@gmail.com>
2024-01-12 14:57:28 +0000

    Add useReducer to implement conditional form rendering to the recipe page

--- src/pages/posts/RecipePage.js
+++ src/pages/posts/RecipePage.js
@@ -8,10 +8,63 @@ import Container from 'react-bootstrap/Container';
 import Header from '../../components/Header';
 import Intro from '../../components/Intro';
 import RatingVote from '../../components/RatingVote';
+import { useCurrentUser } from '../../contexts/CurrentUserContext'; 
+
+const initialState = {
+  recipeData: { results: [] },
+  editingTitle: false,
+  newTitle: '',
+  editingDescription: false,
+  newDescription: '',
+  editingImage: false,
+  newImage: '',
+  editingIngredients: false,
+  newIngredients: '',
+};
+
+const actionTypes = {
+  SET_RECIPE_DATA: 'SET_RECIPE_DATA',
+  SET_EDITING_TITLE: 'SET_EDITING_TITLE',
+  SET_NEW_TITLE: 'SET_NEW_TITLE',
+  SET_EDITING_DESCRIPTION: 'SET_EDITING_DESCRIPTION',
+  SET_NEW_DESCRIPTION: 'SET_NEW_DESCRIPTION',
+  SET_EDITING_IMAGE: 'SET_EDITING_IMAGE',
+  SET_NEW_IMAGE: 'SET_NEW_IMAGE',
+  SET_EDITING_INGREDIENTS: 'SET_EDITING_INGREDIENTS',
+  SET_NEW_INGREDIENTS: 'SET_NEW_INGREDIENTS',
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case actionTypes.SET_RECIPE_DATA:
+      return { ...state, recipeData: action.payload };
+    case actionTypes.SET_EDITING_TITLE:
+      return { ...state, editingTitle: action.payload };
+    case actionTypes.SET_NEW_TITLE:
+      return { ...state, newTitle: action.payload };
+    case actionTypes.SET_EDITING_DESCRIPTION:
+      return { ...state, editingDescription: action.payload };
+    case actionTypes.SET_NEW_DESCRIPTION:
+      return { ...state, newDescription: action.payload };
+    case actionTypes.SET_EDITING_IMAGE:
+      return { ...state, editingImage: action.payload };
+    case actionTypes.SET_NEW_IMAGE:
+      return { ...state, newImage: action.payload };
+    case actionTypes.SET_EDITING_INGREDIENTS:
+      return { ...state, editingIngredients: action.payload };
+    case actionTypes.SET_NEW_INGREDIENTS:
+      return { ...state, newIngredients: action.payload };
+    default:
+      return state;
+  }
+};
 
 function RecipePage() {
+  
   const { id } = useParams();
   const [recipeData, setRecipeData] = useState({ results: [] });
+  const currentUser = useCurrentUser();
+
 
   useEffect(() => {
     const handleMount = async () => {
@@ -34,10 +87,19 @@ function RecipePage() {
     description,
     time_effort,
     ingredients,
-  } = recipeData.results[0] || {};
-  const ingredientsArray = ingredients ? ingredients.split(',').map(item => item.trim()) : [];
-  const headerImageUrl = recipeData.results[0]?.image;
+    } = recipeData.results[0] || {};
+  
+    const ingredientsArray = ingredients ? ingredients.split(',').map(item => item.trim()) : [];
+  
+    const headerImageUrl = recipeData.results[0]?.image;
+  
+    // Declare owner here to avoid the Uncaught ReferenceError
+  const owner = recipeData.results[0]?.owner;
 
+  // Use a different variable name to avoid redeclaration
+  const is_owner = currentUser?.username === owner;
+
+ 
   return (
     <>
       <Header imageUrl={headerImageUrl} />
@@ -53,8 +115,12 @@ function RecipePage() {
       />
       <Row className="justify-content-center">
         <Col className="py-2 p-0 p-lg-2" lg={8}>
-          <Container className="text-center">
+        <Container className="text-center">
+          {is_owner ? (
+            <button>Edit</button>
+          ) : (
             <RatingVote recipeId={id} />
+          )}
           </Container>
           <Container className="text-center">
             Comments
