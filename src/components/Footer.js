9766b163ef53d13da5e8ba39bc3c702fc77ad51d
andreagorsche <andrea.gorsche@gmail.com>
2024-01-27 11:22:01 +0000

    Fix css issues in the navbar and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,32 +1,30 @@
-// Footer.js
 import React from 'react';
-import {Row, Col} from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { FaInstagram, FaFacebook } from 'react-icons/fa';
 import '../styles/components/Footer.module.css';
 
-
 const Footer = () => {
   return (
-    <footer className="footer" style={{ backgroundColor: 'indigo' }}>
+    <footer className="footer" style={{ backgroundColor: 'indigo', color: 'white', padding: '20px' }}>
       <Row>
-      <Col sm={12} md={6} lg={4} style={{ display: 'flex', justifyContent: 'space-around' }} title="Data and Impressum" className="footer-column">
-        <Link to="/impressum" className='footer-text' style={{ color: 'white' }}>Impressum</Link>
-        <Link to="/datenschutz" className='footer-text' style={{ color: 'white' }}>Datenschutz</Link>
-      </Col>
+        <Col xs={12} sm={6} md={4} lg={4} style={{ display: 'flex', justifyContent: 'space-around', alignItems:'center'}}>
+          <Link to="/impressum" style={{ color: 'white' }}>Impressum</Link>
+          <Link to="/datenschutz" style={{ color: 'white' }}>Datenschutz</Link>
+        </Col>
 
-      <Col sm={12} md={6} lg={4} style={{ display: 'flex', justifyContent: 'center'}} title="Copyright" className="footer-column">
-        <p className='footer-text' style={{ color: 'white' }}>&copy; 2023 Cook Around The World. All Rights Reserved.</p>
-      </Col>
+        <Col xs={12} sm={6} md={4} lg={4} style={{ display: 'flex', justifyContent: 'center', alignItems:'center', margin: 0 }} >
+          <p style={{ color: 'white', margin: 0 }}>&copy; 2023 Cook Around The World. All Rights Reserved.</p>
+        </Col>
 
-      <Col sm={12} md={6} lg={4} title="Social Media" style={{ display: 'flex', justifyContent: 'center' }} className="footer-column social-icons">
-        <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
-          <FaInstagram className="social-icon" style={{ color: 'white' }} />
-        </a>
-        <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
-          <FaFacebook className="social-icon" style={{ color: 'white' }} />
-        </a>
-      </Col>
+        <Col xs={12} sm={12} md={4} lg={4} className="footer-column social-icons" style={{ display: 'flex', justifyContent: 'space-around', alignItems:'center' }}>
+          <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
+            <FaInstagram className="social-icon" style={{ color: 'white' }}  />
+          </a>
+          <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
+            <FaFacebook className="social-icon" style={{ color: 'white' }}  />
+          </a>
+        </Col>
       </Row>
     </footer>
   );
