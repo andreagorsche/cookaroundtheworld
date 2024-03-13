234b04ddb7b504d2c350a223ee86d36ff08f859e
andreagorsche <andrea.gorsche@gmail.com>
2024-03-13 16:07:10 +0000

    Add verify email frontend js file

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import Footer from "./components/Footer";
 import FoodFeed from "./pages/posts/FoodFeed";
 import ConfirmationPage from "./pages/auth/ConfirmationPage";
 import InactiveAccount from "./pages/auth/InactiveAccount";
+import VerifyEmail from "./pages/auth/VerifyEmail";
 
 function App() {
 
@@ -35,6 +36,7 @@ function App() {
           <Route exact path="/recipes/:id" render={() => <RecipePage />} />
           <Route exact path ="/register" render = {() => <RegisterForm />}/>
           <Route path="/success" render={() => <ConfirmationPage />}/>
+          <Route path="/verify-email/:key" render={() => <VerifyEmail />}/>
           <Route path="/inactive-account" render={() => <InactiveAccount />}/>
           <Route exact path ="/login" render = {() => <LoginForm />}/>
           <Route render = {()=><p>Page not found!</p>}/>
