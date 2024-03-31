54dc781888b0e45bd6fc654217521b0cd119601b
andreagorsche <andrea.gorsche@gmail.com>
2024-03-31 20:56:56 +0000

    Align login button on confirmation page centrally

--- src/pages/auth/ConfirmationPage.js
+++ src/pages/auth/ConfirmationPage.js
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom';
 import Header from "../../components/Header";
 import JoinHeader from "../../assets/images/JoinHeader.jpg"
 import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+
 
 function ConfirmationPage() {
   return (
@@ -11,10 +13,12 @@ function ConfirmationPage() {
       <h1>Email Confirmation Successful</h1>
       <p>Your email has been successfully verified. You can now access all features of our website.</p>
       <p>You can now proceed to login:</p>
+      <Col className="d-flex justify-content-center">
       <Link to="/login">
-      <Button style={{ backgroundColor: 'indigo', margin: '10px', borderColor: 'transparent', display: 'flex', justifyContent: 'center' }}>        Login
+        <Button style={{ backgroundColor: 'indigo', margin: '10px', borderColor: 'transparent' }}>        Login
         </Button>
       </Link>
+      </Col>
     
     </div>
   );
